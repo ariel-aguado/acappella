f1d36fb94f9b2a85d9029d0b92fe47433c6f5824
Ariel Aguado Albear <ariel.841207@gmail.com>
2025-08-24 16:08:51 -0300

    add about page - :bug: update info and style

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -9,14 +9,38 @@ async function updateSongsData() {
 
 <template>
   <div class="w-full flex flex-col h-[calc(100dvh-64px-52px)] p-4">
-    <div class="flex flex-col items-center border border-accent-content rounded-xl">
-      <Icon name="tabler:brand-netease-music" size="128" />
-      <div class="flex flex-col items-center gap-4 p-4">
-        <span class="leading-6 font-bold text-5xl">
-          Acappella
-        </span>
-        <span class="font-normal text-xl">Himnos Montevideo</span>
+    <div style="width: 100%; height: 300px; position: relative" class="rounded-xl overflow-hidden border border-primary/50">
+      <DarkVeil
+        :hue-shift="0"
+        :noise-intensity="0"
+        :scanline-intensity="0"
+        :speed="1"
+        :scanline-frequency="0"
+        :warp-amount="0"
+        :resolution-scale="1"
+      />
+      <div class="absolute inset-0 flex-1 flex flex-col justify-center items-center text-white border border-white/40 rounded-xl">
+        <Icon name="tabler:brand-netease-music" size="128" />
+        <div class="flex flex-col items-center gap-4 p-4">
+          <span class="leading-6 font-bold text-5xl">
+            Acappella
+          </span>
+          <ShinyText
+            text="Himnos Montevideo"
+            :disabled="false"
+            :speed="3"
+            class-name="font-normal text-xl"
+          />
+        </div>
       </div>
+    </div>
+    <div>
+      <h2 class="text-3xl font-bold mt-8">
+        Acerca de Acappella
+      </h2>
+      <p class="mt-4 leading-8">
+        La aplicación "Acappella" fue creada con el firme propósito de proveer a la Iglesia de Cristo en Montevideo una tecnología dedicada para exaltar el nombre del Señor nuestro Dios, a quien pertenece toda alabanza y honra por la eternidad. Amén.
+      </p>
     </div>
     <div class="flex-1 flex items-end">
       <button
