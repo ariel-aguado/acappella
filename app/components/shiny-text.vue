21ed5a0c2275d5a1c8a652c68fac5c1caf0afa15
Ariel Aguado Albear <46170196+ariel-aguado@users.noreply.github.com>
2025-08-24 16:11:29 -0300

    add about page (#24)

---
+++ app/components/shiny-text.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+type ShinyTextProps = {
+  text: string;
+  disabled?: boolean;
+  speed?: number;
+  className?: string;
+};
+
+const props = defineProps<ShinyTextProps>();
+
+const animationDuration = computed(() => `${props.speed}s`);
+</script>
+
+<template>
+  <div
+    :class="`text-[#b5b5b5a4] bg-clip-text inline-block ${!props.disabled ? 'animate-shine' : ''} ${props.className}`"
+    :style="{
+      backgroundImage:
+        'linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)',
+      backgroundSize: '200% 100%',
+      WebkitBackgroundClip: 'text',
+      animationDuration,
+    }"
+  >
+    {{ props.text }}
+  </div>
+</template>
+
+<style scoped>
+@keyframes shine {
+  0% {
+    background-position: 100%;
+  }
+  100% {
+    background-position: -100%;
+  }
+}
+
+.animate-shine {
+  animation: shine 5s linear infinite;
+}
+</style>
